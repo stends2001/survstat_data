682e197a592383493521fa0f7c71b98ba11826d7
Sten <stends2001@gmail.com>
2025-07-08 11:07:24 +0200

    added documentation to main

--- src/update_survstatdata.py
+++ src/update_survstatdata.py
@@ -5,13 +5,15 @@ from datetime import datetime
 
 def main():
     """
-    The main function with which to download and then preprocess the survstat data.
-    Using current functionality, the function downloads the data for the current year,
-    and then updates the currently existing merged file, with the just now installed year again.
-    The survstat data is updated online each week, so this script could be run each week.
+    This function calls all necessary functions to do the actual legwork.
+    By default, the data for current year is downloaded, and the already
+    existing disease data csv's are updated with this new file.
 
-    Do note that by default, all diseases in log.txt are downloaded and preprocessed. This is quite a lot,
-    so you can also manually specific diseases_dict here.
+    The diseases for which data is downloaded and processed are, by default,
+    extracted from log.txt. Given the shear number of diseases in here, you
+    can adjust the diseases by changing the variables here, or by adjusting
+    log.txt. Do note that by default log.txt is updated depending on the
+    diseases listed.
     """
 
     current_year = datetime.now().year
@@ -22,8 +24,6 @@ def main():
 
     all_years = range(2001, current_year + 1)
 
-    diseases_dict = {'RSV':'rsv'}
-
     scrape_survstat_data(disease_names=diseases_dict, 
                          years=str(current_year), 
                          output_directory=directories_dict['dir_data_raw'], 
@@ -33,7 +33,9 @@ def main():
                              years = str(current_year),
                              raw_data_dir=directories_dict['dir_data_raw'], 
                              processed_data_dir=directories_dict['dir_data_preprocessed'],
-                             how='update') 
+                             how='update')
+
+    
 
     log_script_run(diseases_dict, all_years)
 
